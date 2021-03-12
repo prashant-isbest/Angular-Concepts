968c1e77c48a739ed6693e10a538437f4acd5175
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-12 18:06:36 +0530

    binding to directive properties in this case defaultColor and highlightColor

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
 import { ServerElementComponent } from "./server-element/server-element.component";
 import { CockpitComponent } from "./cockpit/cockpit.component";
 import { BasicHighlightDirective } from "./directives/basic-highlight.directive";
+import { BetterHighlightDirective } from "./directives/better-highlight.directive";
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { BasicHighlightDirective } from "./directives/basic-highlight.directive"
     ServerElementComponent,
     CockpitComponent,
     BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
--- cmp-databinding-start/src/app/directives/better-highlight.directive.ts
+++ cmp-databinding-start/src/app/directives/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from "@angular/core";
@@ -11,20 +12,26 @@ import {
   selector: "[appBetterHighlight]",
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input("appBetterHighlight") defaultColor: string = "transparent";
+  @Input() highlightColor: string = "lightgreen";
   constructor(private elref: ElementRef, private myrenderer: Renderer2) {}
+
   @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
+
   ngOnInit() {
     // this.myrenderer.setStyle(
     //   this.elref.nativeElement,
     //   "backgroundColor",
     //   "lightgreen"
     // );
+    this.backgroundColor = this.defaultColor;
   }
+
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.backgroundColor = "lightgreen";
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.backgroundColor = "pink";
+    this.backgroundColor = this.defaultColor;
   }
 }
