9b010c36930d4d1f4034483d3b11f6c603e5d6a3
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-14 16:25:24 +0530

    styling active Router links and navigating programmatically to servers component using load servers button in home component

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onLoadServers() {
+    this.router.navigate(["/servers"]);
   }
-
 }
