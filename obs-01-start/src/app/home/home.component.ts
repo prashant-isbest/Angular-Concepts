06e6924b5e77218a7607860b6cdee47c15f0fbb6
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-15 13:08:53 +0530

    subscribing and unsubscribing to interval in home component

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor() {}
 
-  constructor() { }
-
+  private obsSubscription: Subscription;
   ngOnInit() {
+    this.obsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy() {
+    this.obsSubscription.unsubscribe();
+  }
 }
