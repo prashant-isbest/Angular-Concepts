aa73d4ee89063c038234c87c9aa7b3c92fec81de
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-15 12:25:01 +0530

    sending a post request and a get request

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -15,11 +15,23 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post(
+        "https://http-angular-f8955-default-rtdb.firebaseio.com/post.json",
+        postData
+      )
+      .subscribe((responeData) => {
+        console.log(responeData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.http
+      .get("https://http-angular-f8955-default-rtdb.firebaseio.com/post.json")
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onClearPosts() {
