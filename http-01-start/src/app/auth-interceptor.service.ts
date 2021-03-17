4082752d25ca3c35454625cb5f68ec4ab9a8f96a
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-17 14:07:32 +0530

    interacting with req and response in interceptor

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -6,10 +6,20 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 
+import { tap } from "rxjs/operators";
 @Injectable()
 export class AuhtInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log("request is on its way");
-    return next.handle(req);
+    console.log(req.url);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append("Auth", "xyz"),
+    });
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log(event.type);
+        console.log(event);
+      })
+    );
   }
 }
