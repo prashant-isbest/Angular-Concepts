94b44958709c5a31cc598243451b62b483acbb6d
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-23 13:25:03 +0530

    registring the control using ngModel , submitting the form and accessing the form with @ViewChild

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild("f") signupForm: NgForm;
+  @ViewChild("inputusername") inputuser: ElementRef;
+
+  ngAfterViewInit() {
+    this.inputuser.nativeElement.focus();
+  }
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+  onSubmit(form: NgForm) {
+    console.log(this.signupForm);
   }
 }
