2fa7ca3e982aca3b663be4b46b791528582479cb
Prashant Joshi <scienceprashant27@gmail.com>
2021-03-23 15:51:50 +0530

    using form status to style the input and outputting validation error message

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { NgForm, NgModel } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -8,6 +8,7 @@ import { NgForm } from "@angular/forms";
 export class AppComponent implements AfterViewInit {
   @ViewChild("f") signupForm: NgForm;
   @ViewChild("inputusername") inputuser: ElementRef;
+  @ViewChild("email") email: NgModel;
 
   ngAfterViewInit() {
     this.inputuser.nativeElement.focus();
@@ -15,7 +16,8 @@ export class AppComponent implements AfterViewInit {
   suggestUserName() {
     const suggestedName = "Superuser";
   }
-  onSubmit(form: NgForm) {
+  onSubmit() {
     console.log(this.signupForm);
+    console.log(this.email);
   }
 }
